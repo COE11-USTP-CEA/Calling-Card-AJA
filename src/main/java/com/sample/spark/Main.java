85406d0fe215b757941c1ec05d05284707480429
Jay Ginete <killer.tilapia@gmail.com>
2017-02-20 08:50:22 +0800

    Fixed minor error in css Fixed formatting

--- src/main/java/com/sample/spark/Main.java
+++ src/main/java/com/sample/spark/Main.java
@@ -11,39 +11,39 @@ public class Main {
 
     public static void main(String[] args) {
         staticFiles.location("/css"); // Static files
-        
+
         get("/home", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("title","Calling Card");
-            return new ModelAndView(model, "home.ftl"); // located in src/test/resources/spark/template/freemarker
+            model.put("title", "Calling Card");
+            return new ModelAndView(model, "home.ftl"); // located in src/main/resources/spark/template/freemarker
         }, new FreeMarkerEngine());
-		
-		get("/about", (req, res) -> {
+
+        get("/about", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("title","About us");
+            model.put("title", "About us");
             model.put("message", "About us!");
-            return new ModelAndView(model, "about.ftl"); // located in src/test/resources/spark/template/freemarker
+            return new ModelAndView(model, "about.ftl"); 
         }, new FreeMarkerEngine());
-		
-		get("/page2", (req, res) -> {
+
+        get("/page2", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("title","page2");
+            model.put("title", "page2");
             model.put("message", "Page2");
-            return new ModelAndView(model, "page2.ftl"); // located in src/test/resources/spark/template/freemarker
+            return new ModelAndView(model, "page2.ftl"); 
         }, new FreeMarkerEngine());
-		
-		get("/page3", (req, res) -> {
+
+        get("/page3", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("title","page3");
+            model.put("title", "page3");
             model.put("message", "Page3");
-            return new ModelAndView(model, "page3.ftl"); // located in src/test/resources/spark/template/freemarker 
+            return new ModelAndView(model, "page3.ftl");
         }, new FreeMarkerEngine());
-		
-		get("/page4", (req, res) -> {
+
+        get("/page4", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("title","page4");
+            model.put("title", "page4");
             model.put("message", "Page4");
-            return new ModelAndView(model, "page4.ftl"); // located in src/test/resources/spark/template/freemarker
+            return new ModelAndView(model, "page4.ftl"); 
         }, new FreeMarkerEngine());
     }
 }
